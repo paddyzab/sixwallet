d4e088ca45a7570fab0d7144fd64a8e4ffaa5a7d
Patryk Zabicki <patryk.zabicki@gmail.com>
2015-03-21 16:10:40 +0100

    mock login menu

--- app/src/main/java/ch/six/sixwallet/Home.java
+++ app/src/main/java/ch/six/sixwallet/Home.java
@@ -20,6 +20,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -138,6 +140,29 @@ public class Home extends Activity implements SwipeRefreshLayout.OnRefreshListen
         super.onResume();
         mGoalView.refresh();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public void onRefresh() {
