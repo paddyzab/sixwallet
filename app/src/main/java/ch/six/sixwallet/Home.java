ae1d884b784f455899c656a174593aee60eec751
Patryk Zabicki <patryk.zabicki@gmail.com>
2015-03-21 16:43:42 +0100

    missing menu.

--- app/src/main/java/ch/six/sixwallet/Home.java
+++ app/src/main/java/ch/six/sixwallet/Home.java
@@ -21,6 +21,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -157,6 +158,13 @@ public class Home extends Activity implements SwipeRefreshLayout.OnRefreshListen
         mGoalView.refresh();
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
